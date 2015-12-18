617450fff5e4e706febe295dbb3e49e331e597ed
JammiM <jammim19@gmail.com>
2015-12-18 10:10:14 +0000

    Adding a fade in feature

--- Jquery/app.js
+++ Jquery/app.js
@@ -1,20 +1,32 @@
+"use strict";
 var main = function () {
- "use strict";
 
  $(".comment-input button").on("click", function (event) {
 	  var $new_comment;
 
  if ($(".comment-input input").val() !== "") {
-	 var $new_comment = $("<p>").text($(".comment-input input").val());
+	 $new_comment = $("<p>").text($(".comment-input input").val());
+	 $new_comment.hide();
 	 $(".comments").append($new_comment);
+	 $new_comment.fadeIn();
 	 $(".comment-input input").val("");
  	}//if
 
  });//onClick
 
 $(".comment-input input").on("keypress", function (event) {
- console.log("this is the keyCode " + event.keyCode);
+ var $new_comment;
+ if (event.keyCode === 13) {
+ if ($(".comment-input input").val() !== "") {
+ var $new_comment = $("<p>").text($(".comment-input input").val());
+ $new_comment.hide();
+ $(".comments").append($new_comment);
+ $new_comment.fadeIn();
+ $(".comment-input input").val("");
+ }
+ }
 });
+
 
 
 
