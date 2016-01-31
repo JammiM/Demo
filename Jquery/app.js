95f3d6dc4f085858aa04b8fa83da7e2b7e166469
JammiM <jammim19@gmail.com>
2016-01-31 00:16:54 +0000

    refactored code

--- Jquery/app.js
+++ Jquery/app.js
@@ -1,42 +1,26 @@
-"use strict";
 var main = function () {
+ "use strict";
+
+var addCommentFromInputBox = function () {
+	 var $new_comment;
+	 if ($(".comment-input input").val() !== "") {
+		 $new_comment = $("<p>").text($(".comment-input input").val());
+		 $new_comment.hide();
+		 $(".comments").append($new_comment);
+		 $new_comment.fadeIn();
+		 $(".comment-input input").val("");
+	 }
+ };
 
  $(".comment-input button").on("click", function (event) {
-	  var $new_comment;
+ 	addCommentFromInputBox();
+ });
 
- if ($(".comment-input input").val() !== "") {
-	 $new_comment = $("<p>").text($(".comment-input input").val());
-	 $new_comment.hide();
-	 $(".comments").append($new_comment);
-	 $new_comment.fadeIn();
-	 $(".comment-input input").val("");
- 	}//if
+ $(".comment-input input").on("keypress", function (event) {
+ 	if (event.keyCode === 13) {
+ 		addCommentFromInputBox();
+ 		}
+ });
 
- });//onClick
-
-$(".comment-input input").on("keypress", function (event) {
- var $new_comment;
- if (event.keyCode === 13) {
- if ($(".comment-input input").val() !== "") {
- var $new_comment = $("<p>").text($(".comment-input input").val());
- $new_comment.hide();
- $(".comments").append($new_comment);
- $new_comment.fadeIn();
- $(".comment-input input").val("");
- }
- }
-});
-
-
-
-
-
-
-
-
-
-
-};//main
-
-
+};
 $(document).ready(main);
