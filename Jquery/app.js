57d7df845d9d8183c6150c32acd2c21c800b6856
JammiM <jammim19@gmail.com>
2016-04-28 12:25:05 +0100

    Cleaned up code and added more comments

--- Jquery/app.js
+++ Jquery/app.js
@@ -1,26 +1,32 @@
+/*  This simple program adds a new comment to
+    the page when the user enters it. 
+*/
 var main = function () {
  "use strict";
 
-var addCommentFromInputBox = function () {
-	 var $new_comment;
-	 if ($(".comment-input input").val() !== "") {
-		 $new_comment = $("<p>").text($(".comment-input input").val());
-		 $new_comment.hide();
-		 $(".comments").append($new_comment);
-		 $new_comment.fadeIn();
-		 $(".comment-input input").val("");
-	 }
- };
+  /*  Add a new comment if the input field is not empty */
+  var addCommentFromInputBox = function () {
+  	var $new_comment;
+  	if ($(".comment-input input").val() !== "") {
+  		 $new_comment = $("<p>").text($(".comment-input input").val());
+  		 $new_comment.hide();
+  		 $(".comments").append($new_comment);
+  		 $new_comment.fadeIn();
+  		 $(".comment-input input").val("");
+  	 }
+  };//addCommentFromInputBox
 
- $(".comment-input button").on("click", function (event) {
- 	addCommentFromInputBox();
- });
+   $(".comment-input button").on("click", function (event) {
+   	addCommentFromInputBox();
+   });
 
- $(".comment-input input").on("keypress", function (event) {
- 	if (event.keyCode === 13) {
- 		addCommentFromInputBox();
- 		}
- });
+   /*   When the enter button is pressed, addCommentFromInputBox is called*/
+   $(".comment-input input").on("keypress", function (event) {
+   	if (event.keyCode === 13) {
+   		   addCommentFromInputBox();
+   		  }
+   });//.comment-input input
 
-};
+};//main
+
 $(document).ready(main);
