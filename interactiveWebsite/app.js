cd7fade6cce35f497f2decaab211c786dddb2094
JammiM <jammim19@gmail.com>
2016-04-29 12:08:50 +0100

    Added JS file and updated code

---
+++ interactiveWebsite/app.js
@@ -0,0 +1,16 @@
+var main = function () {
+
+ "use strict";
+
+ $(".tabs a span").toArray().forEach(function (element) {
+
+     // create a click handler for this element
+     $(element).on("click", function () {
+       $(".tabs a span").removeClass("active");
+       $(element).addClass("active");
+       $("main .content").empty();
+       return false;
+     });
+ 
+ });
+};
