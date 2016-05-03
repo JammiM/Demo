67377bc3a6a0dd3ac3adeffe0998839235f0d236
JammiM <jammim19@gmail.com>
2016-05-03 23:40:42 +0100

    refactoring code

--- interactiveWebsite/app.js
+++ interactiveWebsite/app.js
@@ -1,16 +1,36 @@
 var main = function () {
-
  "use strict";
 
- $(".tabs a span").toArray().forEach(function (element) {
+ var toDos = [
+ "Finish writing this book",
+ "Take Gracie to the park",
+ "Answer emails",
+ "Prep for Monday's class",
+ "Make up some new ToDos",
+ "Get Groceries"
+ ];
+  //Selects all the tabs
+  $(".tabs a span").toArray().forEach(function (element) {
 
-     // create a click handler for this element
-     $(element).on("click", function () {
-       $(".tabs a span").removeClass("active");
-       $(element).addClass("active");
-       $("main .content").empty();
-       return false;
-     });
- 
- });
+    //When the element is clicked
+    $(element).on("click", function () {
+
+      //reference to element
+      var $element = $(element);
+      $(".tabs a span").removeClass("active");
+      $element.addClass("active");
+      $("main .content").empty();
+
+      if ($element.parent().is(":nth-child(1)")) {
+           console.log("FIRST TAB CLICKED!");
+          } else if ($element.parent().is(":nth-child(2)")) {
+              console.log("SECOND TAB CLICKED!");
+          } else if ($element.parent().is(":nth-child(3)")) {
+              console.log("THIRD TAB CLICKED!");
+          }
+      return false;
+    });
+    //click handler
+  });
 };
+$(document).ready(main);
