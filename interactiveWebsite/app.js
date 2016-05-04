d8bde3cd6ebc34fb2dfa436a012086b15eda737f
JammiM <jammim19@gmail.com>
2016-05-05 00:03:10 +0100

    Finishing app.js

--- interactiveWebsite/app.js
+++ interactiveWebsite/app.js
@@ -1,36 +1,62 @@
 var main = function () {
- "use strict";
+    var toDos = ["Get groceries",
+                 "Make up some new ToDos",
+                 "Prep for Monday's class",
+                 "Answer emails",
+                 "Take Gracie to the park",
+                 "Finish writing this book"];
 
- var toDos = [
- "Finish writing this book",
- "Take Gracie to the park",
- "Answer emails",
- "Prep for Monday's class",
- "Make up some new ToDos",
- "Get Groceries"
- ];
-  //Selects all the tabs
-  $(".tabs a span").toArray().forEach(function (element) {
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
 
-    //When the element is clicked
-    $(element).on("click", function () {
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $button,
+                i;
 
-      //reference to element
-      var $element = $(element);
-      $(".tabs a span").removeClass("active");
-      $element.addClass("active");
-      $("main .content").empty();
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
 
-      if ($element.parent().is(":nth-child(1)")) {
-           console.log("FIRST TAB CLICKED!");
-          } else if ($element.parent().is(":nth-child(2)")) {
-              console.log("SECOND TAB CLICKED!");
-          } else if ($element.parent().is(":nth-child(3)")) {
-              console.log("THIRD TAB CLICKED!");
-          }
-      return false;
+            if ($element.parent().is(":nth-child(1)")) {
+                // newest first, so we have to go through
+                // the array backwards
+                $content = $("<ul>");
+                for (i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                // oldest first, so we go through the array forwards
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                // input a new to-do
+                $input = $("<input>"),
+                $button = $("<button>").text("+");
+
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+
+                $content = $("<div>").append($input).append($button);
+               /* Alternatively append() allows multiple arguments so the above
+                can be done with $content = $("<div>").append($input, $button); */
+            }
+
+            $("main .content").append($content);
+
+            return false;
+        });
     });
-    //click handler
-  });
+
+    $(".tabs a:first-child span").trigger("click");
 };
+
 $(document).ready(main);
