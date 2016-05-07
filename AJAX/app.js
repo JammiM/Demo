050e5e697110ffdcd67559c29ff5011704f8f540
JammiM <jammim19@gmail.com>
2016-05-07 15:42:15 +0100

    Appending a JSON array of objects

--- AJAX/app.js
+++ AJAX/app.js
@@ -17,6 +17,26 @@ var main = function () {
 
   });//getJSON
 
+  $.getJSON("cards/hand.json", function (hand) {
+
+   var $list = $("<ul>");
+
+   // hand is an array, so iterate over it using a forEach loop
+   hand.forEach(function (card) {
+
+     // create a list item to hold the card
+     // and append it to the list
+     var $card = $("<li>");
+     $card.text(card.rank + " of " + card.suit);
+     $list.append($card);
+
+   });
+
+   // append the list to main
+   $("main").append($list);
+   
+  });//getJSON
+
 };//main
 
 $(document).ready(main);
