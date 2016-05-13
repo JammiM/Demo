e22326c80c569cc1a9ffa7abab3534b7890a9e99
JammiM <jammim19@gmail.com>
2016-05-13 23:58:17 +0100

    Append images to the DOM

--- FlickrProject/js/app.js
+++ FlickrProject/js/app.js
@@ -1,14 +1,24 @@
 var main = function () {
- "use strict";
 
- var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
+  "use strict";
+
+  var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
 
     $.getJSON(url, function (flickrResponse) {
 
       flickrResponse.items.forEach(function (photo) {
-       console.log(photo.media.m);
-       });
 
+        //Creates el
+        var $img = $("<img>");
+
+        //Sets it's attr
+        $img.attr("src", photo.media.m);
+
+        //Adds it to the main tag
+        $("main .photos").append($img);
+
+        //console.log(photo.media.m);
+      });
 
      console.log(flickrResponse);
    });
